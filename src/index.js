2304c3abfe6836249dd058872e8b0e7c6888af1a
Ash <redcoolike@gmail.com>
2024-11-07 14:14:05 +0100

    initialCommit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export default function orderByProps(obj, order) {
+  const result = [];
+
+  order.forEach((key) => {
+    if (key in obj) {
+      result.push({ key, value: obj[key] });
+    }
+  });
+
+  Object.keys(obj)
+    .filter((key) => !order.includes(key))
+    .sort()
+    .forEach((key) => {
+      result.push({ key, value: obj[key] });
+    });
+
+  return result;
+}
